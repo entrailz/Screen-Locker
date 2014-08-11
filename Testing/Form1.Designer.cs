e36f3df28c450c92ff7d7fb09dd17a01440ecb90
entrailz <entrailz@googlemail.com>
2014-08-11 19:52:39 +0100

    Init commit

---
+++ Testing/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Testing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nsTheme1 = new ClassLibrary2.NSTheme();
+            this.nsButton1 = new ClassLibrary2.NSButton();
+            this.nsTextBox1 = new ClassLibrary2.NSTextBox();
+            this.nsTheme1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nsTheme1
+            // 
+            this.nsTheme1.AccentOffset = 42;
+            this.nsTheme1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.nsTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.nsTheme1.Colors = new ClassLibrary2.Bloom[0];
+            this.nsTheme1.Controls.Add(this.nsButton1);
+            this.nsTheme1.Controls.Add(this.nsTextBox1);
+            this.nsTheme1.Customization = "";
+            this.nsTheme1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nsTheme1.Font = new System.Drawing.Font("Verdana", 8F);
+            this.nsTheme1.Image = null;
+            this.nsTheme1.Location = new System.Drawing.Point(0, 0);
+            this.nsTheme1.Movable = true;
+            this.nsTheme1.Name = "nsTheme1";
+            this.nsTheme1.NoRounding = false;
+            this.nsTheme1.Sizable = true;
+            this.nsTheme1.Size = new System.Drawing.Size(644, 455);
+            this.nsTheme1.SmartBounds = true;
+            this.nsTheme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.nsTheme1.TabIndex = 0;
+            this.nsTheme1.Text = "Computer Lock";
+            this.nsTheme1.TransparencyKey = System.Drawing.Color.Empty;
+            this.nsTheme1.Transparent = false;
+            // 
+            // nsButton1
+            // 
+            this.nsButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nsButton1.Location = new System.Drawing.Point(281, 275);
+            this.nsButton1.Name = "nsButton1";
+            this.nsButton1.Size = new System.Drawing.Size(75, 23);
+            this.nsButton1.TabIndex = 1;
+            this.nsButton1.Text = "    Login";
+            this.nsButton1.Click += new System.EventHandler(this.nsButton1_Click);
+            // 
+            // nsTextBox1
+            // 
+            this.nsTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.nsTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.nsTextBox1.Location = new System.Drawing.Point(0, 220);
+            this.nsTextBox1.MaxLength = 32767;
+            this.nsTextBox1.Multiline = false;
+            this.nsTextBox1.Name = "nsTextBox1";
+            this.nsTextBox1.ReadOnly = false;
+            this.nsTextBox1.Size = new System.Drawing.Size(644, 23);
+            this.nsTextBox1.TabIndex = 0;
+            this.nsTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.nsTextBox1.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 455);
+            this.Controls.Add(this.nsTheme1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.nsTheme1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClassLibrary2.NSTheme nsTheme1;
+        private ClassLibrary2.NSTextBox nsTextBox1;
+        private ClassLibrary2.NSButton nsButton1;
+
+    }
+}
+
---
+++ Testing/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Testing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* Code to Disable WinKey, Alt+Tab, Ctrl+Esc Starts Here */
+
+
+        // Structure contain information about low-level keyboard input event 
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KBDLLHOOKSTRUCT
+        {
+            public Keys key;
+            public int scanCode;
+            public int flags;
+            public int time;
+            public IntPtr extra;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string className, string windowText);
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hwnd, int command);
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindowEx(
+               IntPtr parentHwnd,
+               IntPtr childAfterHwnd,
+               IntPtr className,
+               string windowText);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int WM_APPCOMMAND = 0x319;
+        private string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        //System level functions to be used for hook and unhook keyboard input  
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int id, LowLevelKeyboardProc callback, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hook);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hook, int nCode, IntPtr wp, IntPtr lp);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string name);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern short GetAsyncKeyState(Keys key);
+        //Declaring Global objects     
+        private IntPtr ptrHook;
+        public bool isShown = false;
+        public bool isMuted = false;
+        public IntPtr hwnd = FindWindow("Shell_TrayWnd", "");
+        public IntPtr hwndOrb = FindWindowEx(IntPtr.Zero, IntPtr.Zero, (IntPtr)0xC017, null);
+        public LowLevelKeyboardProc objKeyboardProcess;
+
+        private IntPtr captureKey(int nCode, IntPtr wp, IntPtr lp)
+        {
+            if (nCode >= 0)
+            {
+                KBDLLHOOKSTRUCT objKeyInfo = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lp, typeof(KBDLLHOOKSTRUCT));
+
+                // Disabling Windows keys 
+                if (objKeyInfo.key == Keys.L && (Control.ModifierKeys & Keys.Alt) == Keys.Alt && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+                {
+                    if (isMuted == false)
+                    {
+                        ToggleMute();
+                        isMuted = true;
+                    }
+                    ShowWindow(hwnd, SW_HIDE);
+                    ShowWindow(hwndOrb, SW_HIDE);
+                    KillCtrlAltDelete("1");
+                    isShown = true;
+                    this.Show();
+                }
+                if (isShown)
+                {
+                    if (objKeyInfo.key == Keys.F4 || objKeyInfo.key == Keys.Alt && HasAltModifier(objKeyInfo.flags) ||
+                        objKeyInfo.key == Keys.RWin || objKeyInfo.key == Keys.LWin ||
+                        objKeyInfo.key == Keys.Tab && HasAltModifier(objKeyInfo.flags) ||
+                        objKeyInfo.key == Keys.Escape && (Control.ModifierKeys & Keys.Control) == Keys.Control)
+                    {
+                        return (IntPtr) 1; // if 0 is returned then All the above keys will be enabled
+                    }
+
+                    if (objKeyInfo.key == Keys.Enter)
+                    {
+                        checkPassword(nsTextBox1.Text);
+                    }
+                }
+            }
+            return CallNextHookEx(ptrHook, nCode, wp, lp);
+        }
+
+        bool HasAltModifier(int flags)
+        {
+            return (flags & 0x20) == 0x20;
+        }
+
+        /* Code to Disable WinKey, Alt+Tab, Ctrl+Esc Ends Here */
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            ProcessModule objCurrentModule = Process.GetCurrentProcess().MainModule;
+            objKeyboardProcess = new LowLevelKeyboardProc(captureKey);
+            ptrHook = SetWindowsHookEx(13, objKeyboardProcess, GetModuleHandle(objCurrentModule.ModuleName), 0);
+            this.Hide();
+        }
+
+        private void nsButton1_Click(object sender, EventArgs e)
+        {
+            checkPassword(nsTextBox1.Text);
+        }
+
+        public void checkPassword(string password)
+        {
+            nsTextBox1.Text = "";
+            if (password == "unlockPassword")
+            {
+                ToggleMute();
+                isMuted = false;
+                KillCtrlAltDelete("0");
+                isShown = false;
+                ShowWindow(hwnd, SW_SHOW);
+                ShowWindow(hwndOrb, SW_SHOW);
+                this.Hide();
+            }
+            if (password == "unlockPasswordExit")
+            {
+                ToggleMute();
+                isMuted = false;
+                KillCtrlAltDelete("0");
+                ShowWindow(hwnd, SW_SHOW);
+                ShowWindow(hwndOrb, SW_SHOW);
+                Application.Exit();
+            }
+            else if (password != "")
+            {
+                Log(DateTime.Now + password + "\r\n");
+            }
+        }
+
+        private void ToggleMute()
+        {
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_MUTE);
+        }
+
+        private void Log(string log)
+        {
+            File.AppendAllText(path + "\\login.log", log);
+        }
+        public void KillCtrlAltDelete(string value)
+        {
+            RegistryKey regkey;
+            string keyValueInt = value;
+            string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+
+            if (value == "1")
+            {
+                try
+                {
+                    regkey = Registry.CurrentUser.CreateSubKey(subKey);
+                    regkey.SetValue("DisableTaskMgr", keyValueInt);
+                    regkey.Close();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            else if (value == "0")
+            {
+                try
+                {
+                    regkey = Registry.CurrentUser.OpenSubKey(subKey, true);
+                    regkey.DeleteValue("DisableTaskMgr");
+                    regkey.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
